9e28c7e499500d3a05a14e0f8c62ac8c7283aff5
chenjie <ieachen@163.com>
2022-01-16 20:59:10 +0800

    changes

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ https://vercel.com/docs/runtimes#official-runtimes/python
 https://sspai.com/post/63028
 
 https://towardsdatascience.com/how-to-deploy-a-python-serverless-function-to-vercel-f43c8ca393a0
+
+
+部署到 vercel 时：
+
+- Framework Preset 选 other
+- Build and Output Settings 和 Environment Vaiables 都不用填
+- 直接点部署 Deploy
+
+特别注意：
+
+部署完成后，如果 versel 给的地址是 https://xxx.xxx.vercel.app/ ，直接访问这个地址会失败！
+应当在网址后加上 /api/index，也就是 https://xxx.xxx.vercel.app/api/index ，这样才能访问成功。
--- api/index.py
+++ api/index.py
@@ -1,56 +1,56 @@
 
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
-
-class handler(BaseHTTPRequestHandler):
-
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
-
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
-		return
-        
-
 # from http.server import BaseHTTPRequestHandler
-
+# from urllib import parse
 
 # class handler(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
 
-#         self.send_header('Content-type', 'text/plain')
-#         self.end_headers()
+# 	def do_GET(self):
+# 		s = self.path
+# 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+# 		self.send_response(200)
+# 		self.send_header('Content-type','text/plain')
+# 		self.end_headers()
 
-#         # ################ 输出 json 格式：
-#         # import json
-#         # self.send_header('Content-type', 'application/json')
-#         # self.end_headers()
-#         # data = {'result': 'this is a test'}
-#         # self.wfile.write(json.dumps(data).encode())
+# 		if "name" in dic:
+# 			message = "Hello, " + dic["name"] + "!"
+# 		else:
+# 			message = "Hello, stranger!"
 
-#         # _dict = {
-#         #     'x-forwarded-for': self.headers.get('x-forwarded-for'),
-#         #     'User-Agent': self.headers.get('User-Agent'),
-#         #     'DNT': self.headers.get('DNT'),
-#         # }
+# 		self.wfile.write(message.encode())
+# 		return
+        
 
-#         _dict = dict(self.headers)
-#         print(_dict)
-#         _str = 'The followings are header info: \n\n'
-#         for k, v in _dict.items():
-#             _str += f'{k}: {v} \n'
-#         message_bytes = _str.encode()
+from http.server import BaseHTTPRequestHandler
 
-#         self.wfile.write(message_bytes)
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        # ################ 输出 json 格式：
+        # import json
+        # self.send_header('Content-type', 'application/json')
+        # self.end_headers()
+        # data = {'result': 'this is a test'}
+        # self.wfile.write(json.dumps(data).encode())
+
+        # _dict = {
+        #     'x-forwarded-for': self.headers.get('x-forwarded-for'),
+        #     'User-Agent': self.headers.get('User-Agent'),
+        #     'DNT': self.headers.get('DNT'),
+        # }
+
+        _dict = dict(self.headers)
+        print(_dict)
+        _str = 'The followings are header info: \n\n'
+        for k, v in _dict.items():
+            _str += f'{k}: {v} \n'
+        message_bytes = _str.encode()
+
+        self.wfile.write(message_bytes)
 
 
 # def run():
